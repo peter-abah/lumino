b963125d61e38b58ae866608a0ba7ee9b7237e31
Peter Abah <peterabah.ob@gmail.com>
2024-01-06 14:43:17 +0100

    Add images to hero section and fix navbar sticky bug

--- app/(landing)/components/hero.tsx
+++ app/(landing)/components/hero.tsx
@@ -6,12 +6,14 @@ import { useContext, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { twMerge } from "tailwind-merge";
 import { NavBarStickyContext, NavBarStickyContextType } from "../contexts/nav_bar_sticky_context";
+import Image from "next/image";
 
 const HERO_SLIDES = [
   {
     title: "High-Performance & Elegant Design",
     subtitle: "Introducing the MW08 sport",
     cta: "Shop the MW08 Sport",
+    image: "/images/hero-mw08.jpg",
     ctaLink: "#",
     stylesClasses: {
       containerBg: "bg-hero-mw08",
@@ -24,6 +26,7 @@ const HERO_SLIDES = [
     subtitle: "High-end earphones",
     cta: "Discover collection",
     ctaLink: "#",
+    image: "/images/hero-earphones.jpg",
     stylesClasses: {
       containerBg: "bg-hero-earphones",
       innerBg: "bg-hero-inner-earphones",
@@ -35,6 +38,7 @@ const HERO_SLIDES = [
     subtitle: "High-end headphones",
     cta: "Discover collection",
     ctaLink: "#",
+    image: "/images/hero-headphones.jpg",
     stylesClasses: {
       containerBg: "bg-hero-headphones",
       innerBg: "bg-hero-inner-headphones",
@@ -47,22 +51,23 @@ const HERO_SLIDES = [
 export default function Hero() {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
   const { setIsNavBarSticky } = useContext(NavBarStickyContext) as NavBarStickyContextType;
+  const [previousObserverTop, setPreviousObserverTop] = useState(0);
 
-  // TODO: Document this, explain what is happening and move it to a custom hook
-  const { ref: showStickyNavRef } = useInView({
-    onChange: (inView) => {
-      if (!inView) {
+  // TODO: Maybe move it to a custom hook
+  const { ref: stickyObserverRef } = useInView({
+    onChange: (inView, entry) => {
+      if (entry.boundingClientRect.top < previousObserverTop && !inView) {
+        // User scrolls down and observer is hidden
         setIsNavBarSticky(true);
-      }
-    },
-  });
-  const { ref: hideStickyNavRef } = useInView({
-    onChange: (inView) => {
-      if (inView) {
+      } else if (entry.boundingClientRect.top > previousObserverTop && inView) {
+        // User scrolls up and observer is shown
         setIsNavBarSticky(false);
       }
+
+      setPreviousObserverTop(entry.boundingClientRect.top);
     },
   });
+
   const onSlideButtonClick = (slideIndex: number) => {
     setCurrentSlideIndex(slideIndex);
   };
@@ -79,16 +84,17 @@ export default function Hero() {
         >
           <div
             className={clsx(
-              "relative flex h-[560px] rounded-xl  items-center",
+              "relative flex h-[560px] rounded-xl items-center overflow-hidden",
               slide.stylesClasses.innerBg
             )}
           >
-            <div className="max-w-[40rem] ml-24">
+            <Image src={slide.image} fill alt="" className="object-center object-cover" />
+            <div className="max-w-[40rem] ml-24 z-10">
               <p className="font-bold">{slide.subtitle}</p>
               <h1 className="my-8 text-5xl font-bold">{slide.title}</h1>
               <Link
                 className={clsx(
-                  "inline-block rounded-button px-10 py-4 font-bold",
+                  "inline-block rounded-button px-10 py-4 font-bold hover:bg-opacity-80",
                   slide.stylesClasses.cta
                 )}
                 href={slide.ctaLink}
@@ -106,14 +112,14 @@ export default function Hero() {
         </section>
       ))}
 
-      {/* Observe when element comes into view to show nav bar */}
+      {/* Nav becomes sticky when scrolls down past an observer and changes back to normal when the
+          user scrolls up past the observer. 
+        */}
       <div
-        className="absolute bottom-[40%] h-4 bg-transparent"
+        className="absolute bottom-[40%] h-4 bg-transparent w-8"
         aria-hidden
-        ref={showStickyNavRef}
-      ></div>
-      {/* Observe when element comes into view to show nav bar */}
-      <div className="absolute top-0 h-4 bg-transparent" aria-hidden ref={hideStickyNavRef}></div>
+        ref={stickyObserverRef}
+      />
     </div>
   );
 }
