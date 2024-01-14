522cf41690ef6bd79b613db762597517218a2272
Peter Abah <peterabah.ob@gmail.com>
2024-01-14 15:49:56 +0100

    Fix marquee text

--- app/(landing)/components/marquee_text.tsx
+++ app/(landing)/components/marquee_text.tsx
@@ -1,24 +1,8 @@
-// Used for the marquee animation
-const MARQUEE_TEXT_NO = 4;
-
-// TODO: Not complete at all need to make animation functional, don't understand how to do it yet
 export default function MarqueeText() {
-  const createMarqueeText = () => {
-    const res = [];
-    for (let i = 0; i < MARQUEE_TEXT_NO; i++) {
-      res.push(
-        <p key={i} className="marquee-text">
-          Mastery is a never-ending exploration
-        </p>
-      );
-    }
-    return res;
-  };
-
-  // TODO: Move arbitart values to tailwind
   return (
-    <section className="relative overflow-hidden pb-20 h-[9rem] box-content">
-      {createMarqueeText()}
+    <section className="overflow-hidden pb-20 flex">
+      <span className="marquee-text">Mastery is a never-ending exploration</span>
+      <span className="marquee-text">Mastery is a never-ending exploration</span>
     </section>
   );
 }
