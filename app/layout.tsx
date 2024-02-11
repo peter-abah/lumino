7b5c8280f478dba63b5f9ba96f537bbc84de0372
Peter Abah <peterabah.ob@gmail.com>
2024-02-11 11:32:38 +0100

    Fix path to local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,72 +7,72 @@ import clsx from "clsx";
 const dinNext = localFont({
   src: [
     {
-      path: "./fonts/DIN-Next/DINNextW1G-Italic.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-Italic.otf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-Regular.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-Regular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-UltraLight.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-UltraLight.otf",
       weight: "200",
       style: "normal",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-UltraLightIt.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-UltraLightIt.otf",
       weight: "200",
       style: "italic",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-Light.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-Light.otf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-LightItalic.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-LightItalic.otf",
       weight: "300",
       style: "italic",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-Medium.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-Medium.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-MediumItalic.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-MediumItalic.otf",
       weight: "500",
       style: "italic",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-Bold.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-Bold.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-BoldItalic.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-BoldItalic.otf",
       weight: "700",
       style: "italic",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-Black.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-Black.otf",
       weight: "800",
       style: "normal",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-BlackItalic.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-BlackItalic.otf",
       weight: "800",
       style: "italic",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-Heavy.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-Heavy.otf",
       weight: "900",
       style: "italic",
     },
     {
-      path: "./fonts/DIN-Next/DINNextW1G-HeavyItalic.otf",
+      path: "../fonts/DIN-Next/DINNextW1G-HeavyItalic.otf",
       weight: "900",
       style: "italic",
     },
