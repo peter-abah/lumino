01df231a5e9f403180760a909221cb8df4bc9847
Peter Abah <peterabah.ob@gmail.com>
2024-03-23 20:44:47 +0100

    Indicate if there is are no products available in collection

--- app/collections/[handle]/components/collection_products.tsx
+++ app/collections/[handle]/components/collection_products.tsx
@@ -1,6 +1,7 @@
 import ProductCard from "@/components/product_card";
 import { getCollectionProducts, reshapeProductsToVariantProducts } from "@/lib/shopify";
 import { Collection } from "@/types/shopify";
+import Link from "next/link";
 
 type Props = {
   collection: Collection;
@@ -12,11 +13,22 @@ export default async function CollectionProducts({ collection }: Props) {
 
   return (
     <section className="px-12 mb-20 mt-12 flex flex-col justify-center">
-      <ul className="flex flex-wrap gap-x-6 gap-y-12">
-        {variantProducts.map((product) => (
-          <ProductCard key={product.handle + product.variant.id} product={product} />
-        ))}
-      </ul>
+      {variantProducts.length > 0 ? (
+        <ul className="flex flex-wrap gap-x-6 gap-y-12">
+          {variantProducts.map((product) => (
+            <ProductCard key={product.handle + product.variant.id} product={product} />
+          ))}
+        </ul>
+      ) : (
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <p className="text-2xl md:text-3xl font-bold mb-6 md:mb-8 text-center">
+            No products available at the moment
+          </p>
+          <Link href="/" className="font-bold bg-black text-white px-10 py-4.5 rounded-button">
+            Continue Shopping
+          </Link>
+        </div>
+      )}
     </section>
   );
 }
